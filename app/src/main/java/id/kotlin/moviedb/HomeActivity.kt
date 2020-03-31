6ad4c639ae93a030855fec7f60a1049151b69364
dean.rizz <dean.rizz@ptap.co.id>
2020-04-01 01:46:36 +0700

    Add kotlin android extensions and Refactoring binding view

--- app/src/main/java/id/kotlin/moviedb/HomeActivity.kt
+++ app/src/main/java/id/kotlin/moviedb/HomeActivity.kt
@@ -1,44 +1,46 @@
 package id.kotlin.moviedb
 
 import android.os.Bundle
-import android.telecom.Call
 import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Response
-import javax.security.auth.callback.Callback
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity: AppCompatActivity(), HomeView{
 
-    private lateinit var progressBar: ProgressBar
-    private lateinit var recyclerView: RecyclerView
+    //private lateinit var progressBar: ProgressBar
+    //private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        progressBar = findViewById(R.id.pb_home)
-        recyclerView = findViewById(R.id.rv_home)
+        //progressBar = findViewById(R.id.pb_home)
+        //recyclerView = findViewById(R.id.rv_home)
 
         val presenter = HomePresenter(this)
         presenter.discoverMovie()
     }
 
     override fun onShowLoading() {
-        progressBar.visibility = View.VISIBLE
+        // kotlin android ext
+        pb_home.visibility = View.VISIBLE
+        //progressBar.visibility = View.VISIBLE
     }
 
     override fun onHideLoading() {
-        progressBar.visibility = View.GONE
-        recyclerView.visibility = View.VISIBLE
+        pb_home.visibility = View.GONE
+        rv_home.visibility = View.VISIBLE
+        //progressBar.visibility = View.GONE
+        //recyclerView.visibility = View.VISIBLE
     }
 
     override fun onResponse(result: List<Result>) {
-        recyclerView.addItemDecoration(DividerItemDecoration(this@HomeActivity, DividerItemDecoration.VERTICAL))
-        recyclerView.adapter = HomeAdapter(result)
+        rv_home.addItemDecoration(DividerItemDecoration(this@HomeActivity, DividerItemDecoration.VERTICAL))
+        rv_home.adapter = HomeAdapter(result)
+        //recyclerView.addItemDecoration(DividerItemDecoration(this@HomeActivity, DividerItemDecoration.VERTICAL))
+        //recyclerView.adapter = HomeAdapter(result)
     }
 
     override fun onFailure(error: Throwable) {
